09542c99ecc098f897a14f373fe00f1c38e1d728
dlaborde27 <107769722+dlaborde27@users.noreply.github.com>
2023-06-13 23:48:13 -0500

    cfcf

---
+++ calculadora.js
@@ -0,0 +1,16 @@
+let funcionPredeterminada = () => {
+    const formulario = document.getElementById('formulario')
+    formulario.addEventListener('submit', (evento) => {
+        evento.preventDefault();
+        let valorPorHora = parseFloat(document.getElementById('valorPorHora').value)
+        let interfazHoras = parseFloat(document.getElementById('interfazHoras').value)
+        let estructuraHTML = parseFloat(document.getElementById('estructuraHTMLHoras').value)
+        let estilizacionCSS = parseFloat(document.getElementById('estilizacionCSSHoras').value)
+        let desarrolloJavaScript = parseFloat(document.getElementById('desarrolloJavaScriptHoras').value)
+        let pruebas = parseFloat(document.getElementById('pruebasHoras').value)
+        let resultado = valorPorHora * (interfazHoras+estructuraHTML+estilizacionCSS+desarrolloJavaScript+pruebas)
+        document.getElementById('valorTotal').value = resultado
+      })
+}
+
+funcionPredeterminada()
